28ad23bebbef0b8d19f4c18c0f597f8b98bb7ca9
anamika023 <68707346+anamika023@users.noreply.github.com>
2020-07-29 18:31:45 +0530

    Add files via upload

---
+++ UserService/Controller/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UserSvc.Modal;
+
+namespace UserSvc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserData _userdata;
+
+        public UserController(IUserData userdata)
+        {
+            this._userdata = userdata;
+        }
+       
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return JsonConvert.SerializeObject(_userdata.GetById(id));
+        }
+
+       
+    }
+}
