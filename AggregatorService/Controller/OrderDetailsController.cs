685f0a14c2a394d62580212ab9443e5552729c11
anamika023 <68707346+anamika023@users.noreply.github.com>
2020-07-29 18:08:50 +0530

    Add files via upload

---
+++ AggregatorService/Controller/OrderDetailsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AggregatorService.Modal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AggregatorService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderDetailsController : ControllerBase
+    {
+        private IOrderDetails _orderDetails;
+
+        public OrderDetailsController(IOrderDetails orderDetails)
+        {
+            this._orderDetails = orderDetails;
+        }
+
+        // GET: api/OrderDetails
+        
+        [HttpGet("{id}")]
+        public string Get(int id)
+        
+        {
+            try
+            {
+                var result = _orderDetails.GetOrderDetails(id);
+                return result;
+            }
+
+            catch(Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+
+    }
+
+
+}
